903fc450acd8becc3ed7868c1747d58ad405017c
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-04-14 11:11:46 -0500

    updating php

---
+++ models/pet.php
@@ -0,0 +1,20 @@
+<?php
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+    $dbconn = pg_connect("host=localhost dbname=phpapi");
+}
+ ?>
