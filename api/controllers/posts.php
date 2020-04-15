ec72f16b693d47ed9229ef79c79b9a2005ad4b61
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-04-15 18:10:39 -0500

    more test

--- api/controllers/posts.php
+++ api/controllers/posts.php
@@ -3,7 +3,8 @@ include_once __DIR__ .'/../models/post.php';
 header('Content-Type: application/json');
 
 if ($_REQUEST['action'] === 'index') {
-  echo json_encode(Posts::all());
+  // echo json_encode(Posts::all());
+  echo "hello";
 } elseif ($_REQUEST['action'] === 'post') {
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
