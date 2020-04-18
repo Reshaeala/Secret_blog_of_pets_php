71482906fc9d4b940334e53931c6eab07dd81c97
srl23 <stephen.linfield@comcast.net>
2020-04-17 22:28:22 -0400

    added in vars for the time stamps, likes, and one for the date a post gets edited

--- api/models/post.php
+++ api/models/post.php
@@ -25,12 +25,18 @@ class Post {
   public $avatar;
   public $body;
   public $title;
-  public function __construct($id, $name, $avatar, $body, $title){
+  public $time_stamp;
+  public $last_edited;
+  public $likes;
+  public function __construct($id, $name, $avatar, $body, $title, $time_stamp, $last_edited, $likes){
     $this->id = $id;
     $this->name = $name;
     $this->avatar = $avatar;
     $this->body = $body;
     $this->title = $title;
+    $this->time_stamp = $time_stamp;
+    $this->last_edited = $last_edited;
+    $this->likes = $likes;
   }
 }
 
@@ -47,7 +53,10 @@ class Posts {
         $row_object->name,
         $row_object->avatar,
         $row_object->body,
-        $row_object->title
+        $row_object->title,
+        $row_object->time_stamp,
+        $row_object->last_edited,
+        $row_object->likes
       );
       $posts[] = $new_post;
       $row_object = pg_fetch_object($result);
@@ -59,16 +68,16 @@ class Posts {
   static function create($posts){
 
     $query = "INSERT INTO post (name, title, avatar, body) VALUES ($1, $2, $3, $4)";
-    $query_params = array($posts->name, $posts->avatar, $posts->body, $posts->title);
+    $query_params = array($posts->name, $posts->title, $posts->avatar, $posts->body, $posts->time_stamp, $posts->last_edited, $posts->likes);
     pg_query_params($query, $query_params);
     return self::all();
   }
 
   static function update($updated_post){
-      $query = "UPDATE post SET name = $1, title = $2, avatar = $3, body = $4 WHERE id = $5";
+      $query = "UPDATE post SET name = $1, title = $2, avatar = $3, body = $4, time_stamp = $5, last_edited = $6, likes = $7 WHERE id = $8";
       $query_params = array($updated_post->name,
-      $updated_post->avatar,
-       $updated_post->body, $updated_post->title, $updated_post->id);
+      $updated_post->title,
+       $updated_post->avatar, $updated_post->body, $updated_post->time_stamp, $updated_post->last_edited, $updated_post->likes, $updated_post->id);
       $result = pg_query_params($query, $query_params);
       return self::all();
     }
